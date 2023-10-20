4ef5449b50bfa2da05be8ab84455e206d573de5a
Federico Ziegler <105019871+fedex2118@users.noreply.github.com>
2023-10-20 14:54:41 +0200

    AddedUtenteDAO delete LibONLINEDAO

--- src/dao/UtenteDAO.java
+++ src/dao/UtenteDAO.java
@@ -1,7 +1,11 @@
 package dao;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
+import entities.Recensione;
+import entities.Utente;
 import gestoreConnessioni.GestoreConnessioni;
 import libreriaManager.LibreriaOnline;
 
@@ -10,6 +14,22 @@ public class UtenteDAO {
 		String query = "SELECT * FROM Utente";
 		
 		ResultSet rs = gc.eseguiStatementQuery(query);
+		
+		ArrayList<Utente> utenti = libreria.getUtenti();
+		
+		try {
+			while(rs.next()) {
+				int idUtente = Integer.parseInt(rs.getString(1));
+				String nome = rs.getString(2);
+				String email = rs.getString(3);
+				
+				Utente utente = new Utente(idUtente, nome, email);
+				utenti.add(utente);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		
 	}
